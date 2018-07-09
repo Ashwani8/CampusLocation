f380dbe3ed2a647e0d91176862b0ff14f8470ca3
Ashwani Kumar <akumar@monmouthcollege.edu>
2018-07-09 13:27:21 -0500

    remove GoogleApiClient instead worked with FusedLocationProviderClient

--- app/src/main/java/com/example/admin/campuslocation/MainActivity.java
+++ app/src/main/java/com/example/admin/campuslocation/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.admin.campuslocation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String Log_TAG = MainActivity.class.getSimpleName();
+    private TextView textOutput;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static final int MY_PERMISION_REQUEST_FINE_LOCATION = 101;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textOutput = (TextView) findViewById(R.id.textOutput);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // completed: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // Got last known location. On some rare situations this can be null
+                    if (location != null) {
+                        textOutput.setText(location.toString());
+                    } else {
+                        textOutput.setText("no lication available");
+
+                    }
+                }
+            });
+
+        }   else{
+            // request permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISION_REQUEST_FINE_LOCATION);
+            }
+
+        }
 
     }
 }
+
+
