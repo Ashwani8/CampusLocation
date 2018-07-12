41bf028a122ff11b4f4a334d7dd7a803605b7141
Ashwani Kumar <akumar@monmouthcollege.edu>
2018-07-11 21:22:44 -0500

    changed layout

--- app/src/main/java/com/example/admin/campuslocation/MainActivity.java
+++ app/src/main/java/com/example/admin/campuslocation/MainActivity.java
@@ -5,12 +5,16 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -19,12 +23,21 @@ public class MainActivity extends AppCompatActivity {
     private TextView textOutput;
     private FusedLocationProviderClient mFusedLocationClient;
     private static final int MY_PERMISION_REQUEST_FINE_LOCATION = 101;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private boolean uodateOn = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textOutput = (TextView) findViewById(R.id.textOutput);
+        textOutput = (TextView) findViewById(R.id.detectedActivities);
+        // initialize create object
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000); //  every 10 second
+        mLocationRequest.setFastestInterval(5000); // if another app is using location
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationCallback = new LocationCallback();
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -41,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onSuccess(Location location) {
                     // Got last known location. On some rare situations this can be null
                     if (location != null) {
-                        textOutput.setText(location.toString());
+                        textOutput.setText(String.valueOf(location.getLatitude()));
                     } else {
                         textOutput.setText("no lication available");
 
@@ -57,6 +70,21 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISION_REQUEST_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // the permission is granted, we don't have to do anything.
+                }else {
+                    Toast.makeText(getApplicationContext(),"This app requires locations permissions to be granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
 
